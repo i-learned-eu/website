80d657864a1e18bebb1941ab52f97a3d7bbe0c6e
Eban Rami <contact+git@eban.eu.org>
2022-06-18 16:59:04 +0200

    [⚙️ Minor improvement] Start translation

--- build.py
+++ build.py
@@ -1,9 +1,11 @@
 from staticjinja import Site
+from jinja2 import Environment, FileSystemLoader
 from distutils.dir_util import copy_tree
 import yaml
 import sass
 import os
 import shutil
+from babel.support import Translations
 
 
 if os.path.exists("output") and os.path.isdir("output"):
@@ -11,11 +13,23 @@ if os.path.exists("output") and os.path.isdir("output"):
 with open("./data.yml", 'r') as data:
     try:
         parsedData = yaml.safe_load(data)
-        site = Site.make_site(
-            env_globals=parsedData,
-            outpath="output",
-            )
-        site.render()
+        locale_dir = "translations"
+        msgdomain = "html"
+        list_of_desired_locales = ["en"]
+        loader = FileSystemLoader("templates")
+        extensions = ['jinja2.ext.i18n',
+                      'jinja2.ext.autoescape', 'jinja2.ext.with_']
+
+        translations = Translations.load(locale_dir, list_of_desired_locales)
+        env = Environment(extensions=extensions, loader=loader)
+        env.make_globals(parsedData)
+        env.install_gettext_translations(translations)
+
+        template = env.get_template("index.html")
+        rendered_template = template.render()
+        os.mkdir("output")
+        indexFile = open("output/index.html", "a")
+        indexFile.write(rendered_template)
         os.mkdir("output/static")
         copy_tree("static", "output/static")
         sass.compile(dirname=('output/static/sass',
